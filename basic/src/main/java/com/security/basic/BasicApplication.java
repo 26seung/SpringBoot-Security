fa540f9c28d3cb6aab30daa71b9c5d6b6a0de14f
Euseung <yousi7@naver.com>
2022-09-29 23:59:50 +0900

    first

--- basic/src/main/java/com/security/basic/BasicApplication.java
+++ basic/src/main/java/com/security/basic/BasicApplication.java
@@ -1,13 +1,13 @@
-package com.security.login;
+package com.security.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginApplication {
+public class BasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(BasicApplication.class, args);
 	}
 
 }
--- basic/src/test/java/com/security/basic/BasicApplicationTests.java
+++ basic/src/test/java/com/security/basic/BasicApplicationTests.java
@@ -1,10 +1,10 @@
-package com.security.login;
+package com.security.basic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginApplicationTests {
+class BasicApplicationTests {
 
 	@Test
 	void contextLoads() {
